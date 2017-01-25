7b5d1c5c7b5bdaf851f098f6abb3f9c09c5e63f8
Naveed Hassan <naveed.hassan@nxb.com.pk>
2017-01-25 17:33:11 +0500

    redis integration

--- node/index.js
+++ node/index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Redis = require('ioredis');
+var redis = new Redis();
 
 app.get('/', function (req, res) {
     res.send('index.html');
@@ -8,11 +10,20 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    console.log(arguments);
     socket.on('disconnect', function () {
         console.log('user disconnected');
+        console.log(arguments);
     });
 });
 
+redis.subscribe('test-channel');
+redis.on('message', function (channel, message) {
+    console.log('msg received');
+    console.log(channel);
+    console.log(message);
+});
+
 http.listen(3000, function () {
-    console.log('listening on *:3000');
+    console.log('listening on port 3000');
 });
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,18 @@ Route::get('/job', 'Controller@job');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/redis-pub', function () {
+    $data = [
+        'event' => 'testEvent',
+        'data' => [
+            'id' => 1,
+            'name' => 'Someone',
+        ],
+    ];
+
+    app('redis')->publish('test-channel', json_encode($data));
+
+    dd($data);
+    return 'published';
+});
